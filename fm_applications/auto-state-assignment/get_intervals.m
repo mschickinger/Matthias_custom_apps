6efea3656c76761fdd17038bb8d52b817dfc1c66
mschickinger <m.schickinger@tum.de>
2015-12-10 13:51:37 +0100

    more specificity in adding intervals
    3 types of intervals: bound, unbound and Otsu

--- fm_applications/auto-state-assignment/get_intervals.m
+++ fm_applications/auto-state-assignment/get_intervals.m
@@ -1,4 +1,4 @@
-function [ boundaries, interval_plots ] = get_intervals( traces, ax, varargin )
+function [ boundaries, interval_types, interval_plots ] = get_intervals( traces, ax, varargin )
 %GET_INTERVALS lets the user pick N intervals from a time trace
     % INPUT
     % traces: handle to figure with time trace plots
@@ -21,15 +21,29 @@ function [ boundaries, interval_plots ] = get_intervals( traces, ax, varargin )
     % set parameters
     N = p.Results.N;
     boundaries = zeros(N,2);
+    interval_types = cell(N,1);
     interval_plots = cell(N,1);
+  
+    tmp_type = '';
 
     figure(traces)
     XLIM = get(ax, 'XLim');
     YLIM = get(ax, 'YLim');
 
-    add_button = uicontrol('Style', 'pushbutton', 'String', 'Add',...
-        'Units', 'normalized', 'Position', [.05 .925 .15 .05],... %location, values based on plot_time_traces
-        'Callback', @add); 
+    %Create add_bound button
+    add_bound_button = uicontrol('Style', 'pushbutton', 'String', 'Add bound',...
+        'Units', 'normalized', 'Position', [.075 .925 .15 .05],... %location, values based on plot_time_traces
+        'Callback', @add_bound);
+    
+    %Create add_unbound button
+    add_unbound_button = uicontrol('Style', 'pushbutton', 'String', 'Add UNbound',...
+        'Units', 'normalized', 'Position', [.25 .925 .15 .05],... %location, values based on plot_time_traces
+        'Callback', @add_unbound);
+    
+    %Create add_otsu button
+    add_otsu_button = uicontrol('Style', 'pushbutton', 'String', 'Add for Otsu',...
+        'Units', 'normalized', 'Position', [.425 .925 .15 .05],... %location, values based on plot_time_traces
+        'Callback', @add_otsu);
 
     %Create undo button
     undo_button = uicontrol('Style', 'pushbutton', 'String', 'Undo',...
@@ -49,7 +63,22 @@ function [ boundaries, interval_plots ] = get_intervals( traces, ax, varargin )
     end
     boundaries = boundaries(boundaries(:,1)>0,:);
 
-        function add(source, callbackdata)
+        function add_bound(source, callbackdata)
+            tmp_type = 'b';
+            add;
+        end
+    
+        function add_unbound(source, callbackdata)
+            tmp_type = 'u';
+            add;
+        end
+    
+        function add_otsu(source, callbackdata)
+            tmp_type = 'o';
+            add;
+        end
+    
+        function add
             uiresume(traces)
             h = imrect(ax);
             pos = wait(h);
@@ -61,9 +90,11 @@ function [ boundaries, interval_plots ] = get_intervals( traces, ax, varargin )
             pos(3) = min([XLIM(2)-pos(1)-1 pos(3)]); % Maximum value second to last to avoid indexing error for (forward) delta_pos calculation
             switch n<=N
                 case true
-                    boundaries(n,:) = [pos(1) pos(3)]; 
+                    boundaries(n,:) = [pos(1) pos(3)];
+                    interval_types{n} = tmp_type;
                 case false
                     boundaries = [boundaries; [pos(1) pos(3)]];
+                    interval_types = [interval_types; tmp_type];
             end
             delete(h)
             interval_plots{n} = rectangle('Position', [pos(1) YLIM(1)+.1 pos(3) YLIM(2)-.2], 'EdgeColor', 'b', 'LineWidth', 2);
