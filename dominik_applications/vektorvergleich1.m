ab0a480e376ee741247b10788f0d1dd5471601c4
dvoglmaier <Bjrn's PC@r089184.stusta.swh.mhn.de>
2016-01-13 17:56:15 +0100

    comments and output

--- dominik_applications/vektorvergleich1.m
+++ dominik_applications/vektorvergleich1.m
@@ -2,15 +2,34 @@ function [output] = vektorvergleich1(vektor1,vektor2,toleranz_max)
         
 %{
     input: 
-    
+        - vektor1 enthaelt nur die werte 1 und 2
+        - vektor2 enthaelt nur die werte 1 und 2 und hat die gleiche laenge
+          wie vektor1
+        - toleranz_max kann beliebig gewaehlt werden    
     
     output:
-    
+        - output ist eine cell mit folgender belegung:
+            {1} = anzahl der unbinding event innerhalb der toleranz
+            {2} = anzahl der binding event in. tol.
+            {3} = position der unbinding events in. tol.
+            {4} = position der binding events in. tol.
+            {5} = anzahl der unbinding events ausserhalb der toleranz von
+                  vektor 1
+            {6} = anzahl der binding events auss. tol. von vektor 1
+            {7} = position der unbinding events auss. tol. von vektor 1
+            {8} = position der binding events auss. tol. von vektor 1
+            {9} = anzahl der unbinding events auss. tol. von vektor 2
+            {10} = anzahl der binding events auss. tol. von vektor 2
+            {11} = positionen der unbinding events auss. tol. von vektor 2
+            {12} = positionen der binding events auss. tol. von vektor 2
+            {13} = prozentuale deckung von vektor 1 und vektor 2
+
 %}
     
     %prozentuale deckung der beiden vektoren:
     ueberein = vektor1==vektor2;
     proz_ueberein = (sum(ueberein)*100)/length(ueberein);
+    output{13} = proz_ueberein;
 
     dv1 = vektor1(2:end) - vektor1(1:end-1);
     dv1 = [0,dv1];
@@ -56,14 +75,14 @@ function [output] = vektorvergleich1(vektor1,vektor2,toleranz_max)
         vektor_plot{k} = matrix{k}(find(matrix_in_tol{k}==1));
         %anzahl der events innerhalb der toleranz
         n{k} = length(vektor_plot{k});
-        output{k+2} = n{k};
+        output{k} = n{k};
     end
     
     %OUTPUT:
     %ausgabe der positionen, an denen ein event innerhalb der toleranz in
     %beiden vektoren vorkommt; mithilfe der zeilensummme
     for k = (1:2)
-        output{k} = pos1{k}(find(sum(matrix_in_tol{k},2)==1));
+        output{k+2} = pos1{k}(find(sum(matrix_in_tol{k},2)==1));
     end
     
     %EVENTS AUSSERHALB TOLERANZ:
@@ -76,8 +95,10 @@ function [output] = vektorvergleich1(vektor1,vektor2,toleranz_max)
     for k = (1:2)
         ausser_tol1{k} = sum(find(sum(matrix_in_tol{k},2)==0));
         output{k+4} = ausser_tol1{k};
+        output{k+6} = pos1{k}(find(sum(matrix_in_tol{k},2)==0));
         ausser_tol2{k} = sum(find(sum(matrix_in_tol{k},1)==0));
-        output{k+6} = ausser_tol2{k};
+        output{k+8} = ausser_tol2{k};
+        output{k+10} = pos2{k}(find(sum(matrix_in_tol{k},1)==0));
     end
     
     %hilfsvariablen fuer plots:
