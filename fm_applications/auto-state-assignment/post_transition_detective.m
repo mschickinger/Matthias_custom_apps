64f2c6744ac52f86ebfca81a00549d89ab834de0
hintat <henrikntat@dhcp-10-180-11-101.dynamic.eduroam.mwn.de>
2016-01-14 21:39:36 +0100

    post_transition_detective functionality redundant

--- fm_applications/auto-state-assignment/post_transition_detective.m
+++ fm_applications/auto-state-assignment/post_transition_detective.m
@@ -1,70 +1,96 @@
-function [ eingabe ] = post_transition_detective( eingabe, radius ,rms10)
+function [  ] = post_transition_detective( eingabe, radius ,rms10)
 
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-zahl=0;
-for i=1:length(eingabe)-1
-   if eingabe(i+1)-eingabe(i)~=0
-       zahl=zahl+1;
-   end
-end
-         
     zaehler=1;
-    zustand(zahl,5)=0;
 
     %sigmas entfernung von bound mittel
-    x1=4;
+    x1=3;
     %sigmas entfernung von unbound mittel
     x2=3;
+    %sigmas entfernung zu neuem 
+    z1=1.3;
+    z2=2;
 
     %mindestradius f?r unbound
     y1=0.5;
     %mindestradius f?r bound
     y2=0.5;
-
-    zustand(1,2)=1;
-
-    %?nderung der zust?nde und jeweiligezust?nde merken
-    for i=1:length(eingabe)-1
-        if eingabe(i+1)-eingabe(i)~=0
-            zustand(zaehler,1)=eingabe(i);
-            zustand(zaehler,3)=i;
-            zustand(zaehler+1,2)=i+1;
-            zaehler=zaehler+1;
+    
+    %collect all frames showing beginning of new state
+    zahl=[1;find((eingabe(2:end)-eingabe(1:end-1))~=0)+1];
+        
+    %Matrix for bound;unbound with start/end points, depending on odd/even
+    %number of elements
+    if eingabe(1)==1
+        if rem(length(zahl),2)
+            bound=[zahl(1:2:end)'; (zahl(2:2:end)-1)' length(eingabe)]';
+            unbound=[zahl(2:2:end) zahl(3:2:end)-1];
+        else
+             bound=[zahl(1:2:end) zahl(2:2:end)-1];
+             unbound=[zahl(2:2:end)'; (zahl(3:2:end)-1)' length(eingabe)]';
         end
+        
+    end
+        
+    if eingabe(1)==2
+        if rem(length(zahl),2)
+            unbound=[zahl(1:2:end) zahl(2:2:end)-1];
+            bound=[zahl(2:2:end)'; (zahl(3:2:end)-1)' length(eingabe)]';            
+        else
+            unbound=[zahl(1:2:end)'; (zahl(2:2:end)-1)' length(eingabe)]';
+            bound=[zahl(2:2:end) zahl(3:2:end)-1];
+        end
+        
+    end
+    %mean and deviation of bound;unbound
+    average_bound(size(bound,1))=0;
+    deviation_bound(size(bound,1))=0;
+    
+    average_unbound(size(unbound,1))=0;
+    deviation_unbound(size(unbound,1))=0;
+    %works without for????
+    for i=1:size(bound,1)
+        average_bound(i)=mean(rms10(bound(i,1):bound(i,2)));
+        deviation_bound(i)=std(rms10(bound(i,1):bound(i,2)));
     end
     
-    zustand(end,1)=eingabe(end);
-    zustand(zaehler,3)=length(eingabe);
-     
-    %mittel und std f?r alle zust?nde berechnen
-    for i=1:size(zustand,1)
-        zustand(i,4)=mean(rms10(zustand(i,2):zustand(i,3)));
-        zustand(i,5)=std(rms10(zustand(i,2):zustand(i,3)));
-    end
-    %aussortieren
-    for i=1:size(zustand,1)
-        for j=zustand(i,2):zustand(i,3)
-            %je nach Zustand
-            if zustand(i,1)==1 && rms10(j)>zustand(i,4)+x1*zustand(i,5)
-                for k=j-5:j+5
-                    if radius(k)>y1
-                        eingabe(k)=2;
-                    end
-                end
-            elseif zustand(i,1)==2 && rms10(j)<zustand(i,4)-x2*zustand(i,5)
-                for k=j-4:j+4   
-                    if all(radius(k-1:k+1))<y2
-                        eingabe(k-1:k+1)=1;
-                    end
-                end
-            end
-        end
-    end
-   plot(eingabe,'r');
-   hold on;
-   plot(rms10,'o', 'MarkerSize', 4);
 
+    for i=1:size(unbound,1)
+        average_unbound(i)=mean(rms10(unbound(i,1):unbound(i,2)));
+        deviation_unbound(i)=std(rms10(unbound(i,1):unbound(i,2)));
+    end
+    
+    %potential_unbound declare size before how??
+    for i=1:size(bound,1)
+        potential_unbound=find(rms10(bound(i,1):bound(i,2))>average_bound(i)+x1*deviation_bound(i));
+    end
+    disp(potential_unbound);
+    
+%     %aussortieren
+%     for i=1:size(zustand,1)
+%         for j=zustand(i,2):zustand(i,3)
+%             %je nach Zustand
+%             if zustand(i,1)==1 && rms10(j)>zustand(i,4)+x1*zustand(i,5) && abs(rms10(j)-zustand(i+1,4))<z1*zustand(i+1,5)
+%                 for k=j-5:j+5
+%                     if radius(k)>y1
+%                         eingabe(k)=2;
+%                     end
+%                 end
+%             elseif zustand(i,1)==2 && rms10(j)<zustand(i,4)-x2*zustand(i,5)
+%                 for k=j-4:j+4   
+%                     if all(radius(k-1:k+1))<y2
+%                         eingabe(k-1:k+1)=1;
+%                     end
+%                 end
+%             end
+%         end
+%     end
+% %    plot(eingabe,'r');
+% %    hold on;
+% %    plot(rms10,'o', 'MarkerSize', 4);
+% 
+% disp('zahl');
 end
 
