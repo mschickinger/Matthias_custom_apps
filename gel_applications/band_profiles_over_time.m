caebce63364d7c9dacc901ece11a10f4005a75e5
mschickinger <m.schickinger@tum.de>
2016-01-19 14:23:11 +0100

    Revert "4 bg areas"
    This reverts commit ee11887cde9e9e506d16a40aac52932124994dc1.

--- gel_applications/band_profiles_over_time.m
+++ gel_applications/band_profiles_over_time.m
@@ -20,7 +20,7 @@ gelData_raw = load_gel_image('data_dir', '/Users/matthiasschickinger/PhD/Gels/',
 close all
 
 %% background correct data
-gelData = background_correct_gel_image(gelData_raw, 'numberOfAreas', 4);
+gelData = background_correct_gel_image(gelData_raw, 'numberOfAreas', 1);
 
 %{
 %% adjust of unequal image size
