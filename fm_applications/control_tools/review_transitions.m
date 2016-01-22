7ef1ddd75d23e537235d31cf0b24d82df0e60355
mschickinger <m.schickinger@tum.de>
2016-01-22 13:55:51 +0100

    allow for more transition vectors in spot_data

--- fm_applications/control_tools/review_transitions.m
+++ fm_applications/control_tools/review_transitions.m
@@ -12,14 +12,49 @@ function review_transitions(plot_data, spot_data)
     plot(plot_data.r, 'r.', 'MarkerSize', 8)
     hold on
     plot(plot_data.rms10, 'k-', 'LineWidth', 1.5)
-    for j = spot_data.t_bind'
-        plot([j,j],YLIM,'b-', 'LineWidth', .5);
-        plot(j,.1*YLIM(2),'bv', 'MarkerFaceColor', 'b')
+    
+    % coarse
+    if isfield(spot_data, 't_bind_coarse')
+        for j = spot_data.t_bind_coarse'
+            plot([j,j],YLIM,'b-', 'LineWidth', .5);
+            plot(j,.1*YLIM(2),'bv', 'MarkerFaceColor', 'b')
+        end
     end
-    for j = spot_data.t_unbind'
-        plot([j,j],YLIM,'b-', 'LineWidth', .5);
-        plot(j,.9*YLIM(2),'b^', 'MarkerFaceColor', 'b')
+    if isfield(spot_data, 't_unbind_coarse') 
+        for j = spot_data.t_unbind_coarse'
+            plot([j,j],YLIM,'b-', 'LineWidth', .5);
+            plot(j,.9*YLIM(2),'b^', 'MarkerFaceColor', 'b')
+        end
     end
+    
+    % fine_single
+    if isfield(spot_data, 't_bind_fine_single')
+        for j = spot_data.t_bind_fine_single'
+            plot([j,j],YLIM,'g-', 'LineWidth', .5);
+            plot(j,.1*YLIM(2),'gv', 'MarkerFaceColor', 'b')
+        end
+    end
+    if isfield(spot_data, 't_unbind_fine_single')
+        for j = spot_data.t_unbind_fine_single'
+            plot([j,j],YLIM,'g-', 'LineWidth', .5);
+            plot(j,.9*YLIM(2),'g^', 'MarkerFaceColor', 'b')
+        end
+    end
+
+    % fine_distribution
+    if isfield(spot_data, 't_bind_fine_distribution')
+        for j = spot_data.t_bind_fine_distribution'
+            plot([j,j],YLIM,'c-', 'LineWidth', .5);
+            plot(j,.1*YLIM(2),'cv', 'MarkerFaceColor', 'b')
+        end
+    end
+    if isfield(spot_data, 't_unbind_fine_distribution')
+        for j = spot_data.t_unbind_fine_distribution'
+            plot([j,j],YLIM,'c-', 'LineWidth', .5);
+            plot(j,.9*YLIM(2),'c^', 'MarkerFaceColor', 'b')
+        end
+    end
+
     xlim([first_frame first_frame+w_plot])
     ylim(YLIM)
 
