169742b43447d60ef1cd1b18ad63176f10c42502
mschickinger <matthiasschickinger@fabiansimaclast.dhcp.e14.ph.tum.de>
2016-04-21 14:37:51 +0200

    more rational code

--- fm_applications/auto-state-assignment/chi-squared/remove_lps.m
+++ fm_applications/auto-state-assignment/chi-squared/remove_lps.m
@@ -11,43 +11,40 @@ function [ steps_out ] = remove_lps( trace, steps_in, varargin )
 
     parse(p,trace, steps_in, varargin{:});
     
+    % preparation of data
     switch isempty(p.Results.interval)
         case true
             trace = p.Results.trace;
-            steps_out = p.Results.steps_in;
-            delta_chi2 = zeros(size(steps_out));
-            A = mean(trace(steps_out(1):steps_out(2)-1));
-            B = mean(trace(1:steps_out(2)-1));
-            delta_chi2(1) = (A-B)^2; 
-            for i = 2:length(delta_chi2)-1
-                A = mean(trace(steps_out(i):steps_out(i+1)-1));
-                B = mean(trace(steps_out(i-1):steps_out(i+1)-1));
-                delta_chi2(i) = (A-B)^2;
-            end
-            A = mean(trace(steps_out(end):end));
-            B = mean(trace(steps_out(end-1):end));
-            delta_chi2(end) = (A-B)^2; 
-            
-            [~, mindex] = min(delta_chi2);
-            steps_out(mindex) = []; 
+            tmp_steps = p.Results.steps_in;
         case false
             interval = p.Results.interval;
             trace = p.Results.trace(interval(1):interval(2));
             tmp_steps = p.Results.steps_in(p.Results.steps_in>=interval(1) ...
                 & p.Results.steps_in<=interval(2)) - interval(1) + 1;
-            delta_chi2 = zeros(size(tmp_steps));
-            A = mean(trace(tmp_steps(1):tmp_steps(2)-1));
-            B = mean(trace(1:tmp_steps(2)-1));
-            delta_chi2(1) = (A-B)^2; 
-            for i = 2:length(delta_chi2)-1
-                A = mean(trace(tmp_steps(i):tmp_steps(i+1)-1));
-                B = mean(trace(tmp_steps(i-1):tmp_steps(i+1)-1));
-                delta_chi2(i) = (A-B)^2;
-            end
-            A = mean(trace(tmp_steps(end):end));
-            B = mean(trace(tmp_steps(end-1):end));
-            delta_chi2(end) = (A-B)^2; 
-            [~, mindex] = min(delta_chi2);
+    end
+    
+    % main part: decide which step to kill
+    delta_chi2 = zeros(size(tmp_steps));
+    A = mean(trace(tmp_steps(1):tmp_steps(2)-1));
+    B = mean(trace(1:tmp_steps(2)-1));
+    delta_chi2(1) = (A-B)^2; %difference in variances with/without step (neglecting sub-interval before step)
+    for i = 2:length(delta_chi2)-1
+        A = mean(trace(tmp_steps(i):tmp_steps(i+1)-1));
+        B = mean(trace(tmp_steps(i-1):tmp_steps(i+1)-1));
+        delta_chi2(i) = (A-B)^2;
+    end
+    A = mean(trace(tmp_steps(end):end));
+    B = mean(trace(tmp_steps(end-1):end));
+    delta_chi2(end) = (A-B)^2; %difference in variances with/without step (neglecting sub-interval before step)
+
+    [~, mindex] = min(delta_chi2);
+    
+    % create output
+    switch isempty(p.Results.interval)
+        case true
+            tmp_steps(mindex) = [];
+            steps_out = tmp_steps;
+        case false
             steps_out = p.Results.steps_in(p.Results.steps_in ~= ...
                 tmp_steps(mindex) + interval(1) - 1);
     end
