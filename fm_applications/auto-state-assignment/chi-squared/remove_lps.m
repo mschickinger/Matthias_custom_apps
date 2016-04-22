99e995ede0526773c1b59aebec3b24274ac85995
mschickinger <matthiasschickinger@fabiansimaclast.dhcp.e14.ph.tum.de>
2016-04-22 10:17:59 +0200

    decision for step removal: step size
    see comments

--- fm_applications/auto-state-assignment/chi-squared/remove_lps.m
+++ fm_applications/auto-state-assignment/chi-squared/remove_lps.m
@@ -22,22 +22,34 @@ function [ steps_out ] = remove_lps( trace, steps_in, varargin )
             & p.Results.steps_in<=interval(2)) - interval(1) + 1;
     end
     
-    % main part: decide which step to kill
-    delta_chi2 = zeros(size(tmp_steps));
-    A = mean(trace(tmp_steps(1):tmp_steps(2)-1));
-    B = mean(trace(1:tmp_steps(2)-1));
-    delta_chi2(1) = (A-B)^2; %difference in variances with/without step (neglecting sub-interval before step)
-    for i = 2:length(delta_chi2)-1
-        A = mean(trace(tmp_steps(i):tmp_steps(i+1)-1));
-        B = mean(trace(tmp_steps(i-1):tmp_steps(i+1)-1));
-        delta_chi2(i) = (A-B)^2;
-    end
-    A = mean(trace(tmp_steps(end):end));
-    B = mean(trace(tmp_steps(end-1):end));
-    delta_chi2(end) = (A-B)^2; %difference in variances with/without step (neglecting sub-interval before step)
-
-    [~, mindex] = min(delta_chi2);
+    % MAIN PART: DECIDE WHICH STEP TO KILL
     
+    % experiment: decide based on changes in total variance upon step
+    % removal (did not work so well)
+%     delta_chi2 = zeros(size(tmp_steps));
+%     A = sum((trace(1:tmp_steps(1)-1) - mean(trace(1:tmp_steps(1)-1))).^2);
+%     B = sum((trace(tmp_steps(1):tmp_steps(2)-1) - mean(trace(tmp_steps(1):tmp_steps(2)-1))).^2);
+%     C = sum((trace(1:tmp_steps(2)-1) - mean(trace(1:tmp_steps(2)-1))).^2);
+%     delta_chi2(1) = (A+B)/C; %difference in variances with/without step (neglecting sub-interval before step)
+%     for i = 2:length(delta_chi2)-1
+%         A = mean(trace(tmp_steps(i):tmp_steps(i+1)-1));
+%         B = mean(trace(tmp_steps(i-1):tmp_steps(i+1)-1));
+%         A = sum((trace(tmp_steps(i-1):tmp_steps(i)-1) - mean(trace(tmp_steps(i-1):tmp_steps(i)-1))).^2);
+%         B = sum((trace(tmp_steps(i):tmp_steps(i+1)-1) - mean(trace(tmp_steps(i):tmp_steps(i+1)-1))).^2);
+%         C = sum((trace(tmp_steps(i-1):tmp_steps(i+1)-1) - mean(trace(tmp_steps(i-1):tmp_steps(i+1)-1))).^2);
+%         delta_chi2(1) = (A+B)/C;
+%     end
+%     A = sum((trace(tmp_steps(end-1):tmp_steps(end)-1) - mean(trace(tmp_steps(end-1):tmp_steps(end)-1))).^2);
+%     B = sum((trace(tmp_steps(end):end) - mean(trace(tmp_steps(end):end))).^2);
+%     C = sum((trace(tmp_steps(end-1):end) - mean(trace(tmp_steps(end-1):end))).^2);
+%     delta_chi2(1) = (A+B)/C;
+% 
+%     [~, mindex] = min(delta_chi2);
+    
+    % decide based on total step size (difference of mean levels)
+    tmp_levels = get_levels_trace(trace, tmp_steps);
+    [~, mindex] = min(abs(tmp_levels(2:end)-tmp_levels(1:end-1)));
+   
     % create output
     switch isempty(p.Results.interval)
         case true
