29721634df35c7dcfbc6724b5f2ebe25e10a703a
mschickinger <matthiasschickinger@giskardeventlov.dhcp.e14.ph.tum.de>
2016-04-28 20:59:44 +0200

    find true global minimum
    this yields a much better result, but also needs a lot more computation
    time. think about ways to do this more economically!

--- fm_applications/auto-state-assignment/chi-squared/find_2mps.m
+++ fm_applications/auto-state-assignment/chi-squared/find_2mps.m
@@ -8,28 +8,35 @@ function [step_pos, delta, chi2] = find_2mps(vector, varargin)
     parse(p, vector, varargin{:});
     
     max_frame = length(vector);
-
-    chi2 = zeros(2,max_frame);
-    chi2(1,1) = sum((vector-mean(vector)).^2);
-    chi2(1,end) = chi2(1,1);
-
-    for i = 2:max_frame-1
-        chi2(1,i) = sum((vector(1:i-1)-mean(vector(1:i-1))).^2) + sum((vector(i:max_frame)-mean(vector(i:max_frame))).^2);
-    end
     
-    chi2_min = zeros(2,1);
+    tmp_chi2 = sum((vector-mean(vector)).^2);
     step_pos = zeros(2,1);
     delta = zeros(2,1);
-
-    [chi2_min(1), step_pos(1)] = min(chi2(1,:));
+    step_pos(1) = 1;
+    step_pos(2) = max_frame;
+    
+    for i = 2:max_frame-1
+        for j=i+1:max_frame
+            tmp = sum((vector(1:i-1)-mean(vector(1:i-1))).^2) + ...
+                    sum((vector(i:j-1)-mean(vector(i:j-1))).^2) + ...
+                    sum((vector(j:max_frame)-mean(vector(j:max_frame))).^2);
+            if  tmp < tmp_chi2
+                tmp_chi2 = tmp;
+                step_pos = [i;j];
+            end
+        end
+    end
     delta(1) = max(vector(max(step_pos(1)-5,1):min(step_pos(1)+4,length(vector))))-min(vector(max(step_pos(1)-5,1):min(step_pos(1)+4,length(vector))));
+    delta(2) = max(vector(max(step_pos(2)-5,1):min(step_pos(2)+4,length(vector))))-min(vector(max(step_pos(2)-5,1):min(step_pos(2)+4,length(vector))));
+
+    chi2 = zeros(2,max_frame);
     
     for i = 1:max_frame
+        chi2(1,i) = get_chi2(vector, [i step_pos(2)]);
         chi2(2,i) = get_chi2(vector, [step_pos(1) i]);
     end
     
-    [chi2_min(2), step_pos(2)] = min(chi2(2,:));
-    delta(2) = max(vector(max(step_pos(2)-5,1):min(step_pos(2)+4,length(vector))))-min(vector(max(step_pos(2)-5,1):min(step_pos(2)+4,length(vector))));
+    chi2_min = min(chi2,[],2);
     
     if p.Results.plot
         if strcmp(questdlg('Close all open figures?', 'Close all?', 'Yes'), 'Yes')
