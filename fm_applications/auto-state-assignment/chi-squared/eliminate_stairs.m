662711aae25bb9ff9b244b0fef0dc12a31413fdc
mschickinger <matthiasschickinger@Giskard-Reventlov.local>
2016-08-02 17:50:35 +0200

    keep eliminating until all stairs are gone

--- fm_applications/auto-state-assignment/chi-squared/eliminate_stairs.m
+++ fm_applications/auto-state-assignment/chi-squared/eliminate_stairs.m
@@ -18,34 +18,44 @@ function [ steps_no_stairs ] = eliminate_stairs ( trace, steps, varargin )
     
     % up and down steps
     dv = sign(levels(2:end)-levels(1:end-1));
-    steps_cell{2} = p.Results.steps(dv==-1); % downward steps
-    steps_cell{1} = p.Results.steps(dv==1); % upward steps
+    steps_cell{2} = reshape(p.Results.steps(dv==-1),sum(dv==-1),1); % downward steps
+    steps_cell{1} = reshape(p.Results.steps(dv==1),sum(dv==1),1); % upward steps
+    L = length(steps_cell{1}) + length(steps_cell{2});
     
     % remove consecutive same direction steps
-    for s = 1:2
-        % first entry (i=1)
-        tmp_ceil = min(steps_cell{1+(s==1)}(steps_cell{1+(s==1)}>steps_cell{s}(1)));
-        if isempty(tmp_ceil)
-            tmp_ceil = length(trace)+1;
-        end
-        while sum(steps_cell{s}>=steps_cell{s}(1) & steps_cell{s}<tmp_ceil)>1
-            steps_cell{s} = remove_lps(trace, steps_cell{s}, [1 tmp_ceil-1]);
-        end
-        
-        i = 2;
-        while i < length(steps_cell{s})
-            tmp_floor = max(steps_cell{1+(s==1)}(steps_cell{1+(s==1)}<steps_cell{s}(i)));
-            tmp_ceil = min(steps_cell{1+(s==1)}(steps_cell{1+(s==1)}>steps_cell{s}(i)));
+    go_on = 1;
+    while go_on
+        for s = 1:2
+            % first entry (i=1)
+            tmp_ceil = min(steps_cell{1+(s==1)}(steps_cell{1+(s==1)}>steps_cell{s}(1)));
             if isempty(tmp_ceil)
                 tmp_ceil = length(trace)+1;
             end
-            while sum(steps_cell{s}>tmp_floor & steps_cell{s}<tmp_ceil)>1
-                steps_cell{s} = remove_lps(trace, steps_cell{s}, [tmp_floor tmp_ceil-1]);
+            while sum(steps_cell{s}>=steps_cell{s}(1) & steps_cell{s}<tmp_ceil)>1
+                steps_cell{s} = remove_lps(trace, steps_cell{s}, [1 tmp_ceil-1]);
             end
-            i = i+1;
+
+            i = 2;
+            while i < length(steps_cell{s})
+                tmp_floor = max(steps_cell{1+(s==1)}(steps_cell{1+(s==1)}<steps_cell{s}(i)));
+                tmp_ceil = min(steps_cell{1+(s==1)}(steps_cell{1+(s==1)}>steps_cell{s}(i)));
+                if isempty(tmp_ceil)
+                    tmp_ceil = length(trace)+1;
+                end
+                while sum(steps_cell{s}>tmp_floor & steps_cell{s}<tmp_ceil)>1
+                    steps_cell{s} = remove_lps(trace, steps_cell{s}, [tmp_floor tmp_ceil-1]);
+                end
+                i = i+1;
+            end
+        end
+        steps_no_stairs = sort(vertcat(steps_cell{:}));
+        go_on = L > length(steps_no_stairs);
+        if go_on
+            levels = get_levels(trace, steps_no_stairs);
+            dv = sign(levels(2:end)-levels(1:end-1));
+            steps_cell{2} = steps_no_stairs(dv==-1); % downward steps
+            steps_cell{1} = steps_no_stairs(dv==1); % upward steps
+            L = length(steps_no_stairs);
         end
     end
-    
-    % create output
-    steps_no_stairs = sort(vertcat(steps_cell{:}));
 end
