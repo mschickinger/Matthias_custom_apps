1afae85fe4c9855eb4604b122d29ebfdb8baf512
mschickinger <matthiasschickinger@Giskard-Reventlov.local>
2016-08-15 13:04:22 +0200

    GO_ON if-statements within todo if loop
    meant to avoid that all following todos that were already 0 are set to
    2.

--- fm_applications/steps_by_size/main_script.m
+++ fm_applications/steps_by_size/main_script.m
@@ -45,13 +45,14 @@ for m = 1:size(hop.results,1)
                 end
             end
             hop.results{m}{s}.todo = uint8(~GO_ON);
-        end
-        if GO_ON == 0
-            hop.results{m}{s}.todo = int8(~strcmp(questdlg('Also save last viewed trace?','Save last trace?'),'Yes'));
-            uisave({'hop','rms_cell'}, 'hop.mat')
-            return
-        elseif GO_ON == 2
-            hop.results{m}{s}.todo = uint8(2);
+            if GO_ON == 0
+                hop.results{m}{s}.todo = int8(~strcmp(questdlg('Also save last viewed trace?','Save last trace?'),'Yes'));
+                uisave({'hop','rms_cell'}, 'hop.mat')
+                return
+            elseif GO_ON == 2
+                hop.results{m}{s}.todo = uint8(2);
+                GO_ON = 1;
+            end
         end
     end
 end
