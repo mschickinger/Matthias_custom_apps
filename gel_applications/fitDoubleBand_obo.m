6c06f26919f4d501164472d4f83c3e25614bd0ce
mschickinger <matthiasschickinger@Giskard-Reventlov.local>
2016-09-21 15:08:24 +0200

    cleanup

--- gel_applications/fitDoubleBand_obo.m
+++ gel_applications/fitDoubleBand_obo.m
@@ -9,7 +9,7 @@ function output = fitBands_obo( prfl )
     h = imrect(gca);
     setResizable(h,'true');
     pos1 = round(wait(h));
-    fit1 = fitMainBand(prfl, pos1(1), pos1(3));
+    fit1 = fitSingleBand(prfl, pos1(1), pos1(3));
     f1 = fit1.fit_g1(1:length(prfl));
     plot(prfl, 'o', 'MarkerSize', 8)
     hold on
@@ -20,7 +20,7 @@ function output = fitBands_obo( prfl )
     h = imrect(gca);
     setResizable(h,'true');
     pos2 = round(wait(h));
-    fit2 = fitMainBand(prfl-f1, pos2(1), pos2(3));
+    fit2 = fitSingleBand(prfl-f1, pos2(1), pos2(3));
     f2 = fit2.fit_g1(1:length(prfl));
     plot(prfl-f1, 'o', 'MarkerSize', 8)
     hold on
@@ -32,7 +32,7 @@ function output = fitBands_obo( prfl )
 
     lower = min(pos1(1),pos2(1));
     width = max(pos1(1)+pos1(3),pos2(1)+pos2(3))-lower;
-    start = [1.5*fit2.fit_g1.a1 fit2.fit_g1.b1 fit2.fit_g1.c1 fit1.fit_g1.a1/1.5 fit1.fit_g1.b1 fit1.fit_g1.c1];
+    start = [fit2.fit_g1.a1 fit2.fit_g1.b1 fit2.fit_g1.c1 fit1.fit_g1.a1 fit1.fit_g1.b1 fit1.fit_g1.c1];
     fit3 = fitDoubleBand(prfl, lower, width, 'start', start);
     f3 = fit3.fit_g2(1:length(prfl));
 
