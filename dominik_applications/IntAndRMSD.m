b25d59ed934c881de027a4e3d1179303230cb57f
dvoglmaier <dominik.studium@gmx.de>
2016-11-17 13:28:26 +0100

    correct version

--- dominik_applications/IntAndRMSD.m
+++ dominik_applications/IntAndRMSD.m
@@ -5,7 +5,7 @@ function [output] = IntAndRMSD(data,vector)
 %           vector      Ausgewaehlten traces angibt, mit [movie spot; movie spot; ...]
 
 % output:   struct mit folgenden Unterpunkten:
-%           .interval   enthaelt die Intensitaets- und RMSD-Werte des Intervals
+%           .interval   enthaelt die Intensitaets- und RMSD-Werte des Intervalls
 %                       sortiert nach Intensitaet
 %           .median     enthaelt den Medianwert des jeweiligen Intervals
 %           .minmax     enthaelt zwei Eintraege, oben min und unten max des
@@ -15,9 +15,7 @@ function [output] = IntAndRMSD(data,vector)
 %           .center     enthaelt die mittleren 95 Prozent der RMSD-Werte des
 %                       jeweiligen Intervals
 %           .midpoint   enthaelt den jeweiligen
-%                       Intensitaetintervallmittelpunkt
-
-
+%                       Intensitaetintervallmittelpunkte
 
 %% parameters
 chm = 1; % mobile channel: Rot
@@ -36,6 +34,7 @@ end
 all_ints = cell(size(indices));
 all_rms10 = cell(size(indices));
 counter = 0;
+intervals_comb = []; %zeros(length(vector)*27000,2); % koennte man auch sehr sehr gross machen und am Ende alle Nullen wieder herausloeschen, kA wie man sonst die Vektorgroesse sonst vorher angeben kann...
 for m = 1:length(indices)
     all_ints{m} = zeros(sum(nFrames{m}),1);
     all_rms10{m} = zeros(sum(nFrames{m}),1);
@@ -43,87 +42,28 @@ for m = 1:length(indices)
         all_ints{m}(counter+(1:nFrames{m}(s))) = data{m}{indices{m}(s),chm}.itrace(1:nFrames{m}(s));
         all_rms10{m}(counter+(1:nFrames{m}(s))) = data{m}{indices{m}(s),chm}.vwcm.rms10(1:nFrames{m}(s));
         counter = counter + nFrames{m}(s);
+        intervals_comb = [intervals_comb; all_ints{m} all_rms10{m}];
     end
 end
-
-%% intervals
-intervals_comb = [];
-for i = 1:length(all_ints)
-    intervals_comb = [intervals_comb; all_ints{i} all_rms10{i}]; % 2 Spalten Vektor mit Intensitaets-(1) und RMSD-Werten(2)
-end
-intervals_comb = sortrows(intervals_comb,1); % nach Intensitaet sortiert; MS: Dieser Sortierschritt ist unnoetig.
-% Ueberhaupt kannst du diesen Teil schon durch umschreiben der vorherigen
-% section erledigen. die all_ints und all_rms10 werden spaeter ja eh nicht
-% mehr verwendet, oder?
-
 intervals_comb = intervals_comb(intervals_comb(:,1)>0,:); % alle Null-Eintraege werden wieder entfernt (keine Ahnung woher diese kommen)
 
-%MS: Ich haette das gern so, dass am Rand abgeschnitten wird, also wirklich
-%nur die Punkte mitgenommen werden, die in einem der vorgegebenen
-%Intervalle liegen. Ausserdem: Was passiert hier mit den Werten, die genau
-%auf einer Intervallgrenze liegen?
-
-output_intervals = cell(1,length(intervals)+1);
-for i = 1 % erstes Intervall liegt links der ersten Grenze
-    output_intervals{i} = intervals_comb(intervals_comb(:,1)<intervals(i),:);
-end
-for i = 2:length(intervals) % Intervalle nach den vorgegebenen Breiten
-    output_intervals{i} = intervals_comb(intervals_comb(:,1)>intervals(i-1)&intervals_comb(:,1)<intervals(i),:);
-end
-for i = length(intervals)+1 % letztes Intervall liegt rechts von letzter Grenze; bei i Unterteilungen gibt es i+1 Intervalle!
-    output_intervals{i} = intervals_comb(intervals_comb(:,1)>intervals(i-1),:);
+%% intervals
+midpoints = 5100:200:13900; % 45 Mittelpunkte entsprechen 45 Intervallen
+output_intervals = cell(1,length(midpoints));
+for i = 1:length(midpoints) % z.B.: Intervall 1 entspricht 5000 bis 5199
+    output_intervals{i} = intervals_comb(intervals_comb(:,1) >= (midpoints(i)-100) & intervals_comb(:,1) <= (midpoints(i)+99),:);
 end
 
-%MS: probier doch mal aus, um welchen Faktor sich die Rechenzeit
-%reduziert, wenn du die folgenden drei oder vier for-Schleifen zu einer
-%Schleife kombinierst. Das geht z.B. mit tic und toc
-%% median
-output_median = cell(1,length(intervals)+1);
-for i = 1:length(intervals)+1
-    output_median{i} = median(output_intervals{i}(:,2));
-end
-
-%% Max and Min
-output_minmax = cell(1,length(intervals)+1);
-for i = 1:length(intervals)+1
-    output_minmax{i} = [min(output_intervals{i}(:,2));max(output_intervals{i}(:,2))];
-end
-
-%% Interquartils-distance
-output_iqr = cell(1,length(intervals)+1);
-for i = 1:length(intervals)+1
-    output_iqr{i} = iqr(output_intervals{i}(:,2));
-end
-
-%% center 95 percent
-% MS: Hier hatte ich mich evtl. unklar ausgedrueckt. Es reichen mir die
-% obere und untere Grenze des 95%-Intervalls, also diese ceil-/floor-Werte.
-% Sollte aber auch mit dem Befehl prctile gehen, oder?
-
-output_center = cell(1,length(intervals)+1);
-for i = 1:length(intervals)+1
-    if ~isempty(output_intervals{i})
-        variable = []; % Hilfsvariable
-        variable = output_intervals{i};
-        variable = sortrows(variable,2); % das Intervall nach RMSD-Werten sortieren und nur 95 Prozent auswaehlen
-        %MS: Gute Idee mit der Hilfsvariable, aber aus den vorherigen drei Schritten kannst du einen einzigen
-        %machen. Und es empfiehlt sich, solche Hilfsvariablen mit "tmp" zu
-        %benennen. Dann ist gleich klar, dass es nur eine 'Zwischenablage' ist.
-        variable = variable(ceil(length(variable)*0.025):floor(length(variable)*0.975),:);   
-        output_center{i} = sortrows(variable,1); % wieder nach Intensitaet sortieren; MS: sind sie das nicht schon?!
-    end
-end
-
-%% interval midpoint
-output_midpoints = cell(1,length(intervals)+1); %MS: Fuer einfache Zahlenwerte brauchst du kein cell array, da tut es auch ein normales array.
-output_midpoints{1} = [];
-output_midpoints{end} = [];
-abs = intervals(2)-intervals(1); %MS: Achtung mit abs, das gibt es schon als Befehl in Matlab (absolute value).
-%Hier wuerde auch 'delta' passen. Oder 'width'/'w'/'hw'. Und wenn du es gleich
-%halbierst, dann musst du das nicht in jeder Schleifeniteration tun. Wobei
-%es eh auch ohne Schleife geht, wenn du ein normales array benutzt.
-for i = 2:length(intervals)
-    output_midpoints{i} = intervals(i)-abs*0.5; % Mittelpunkte nur von Intervall i+1 bis i-1, da sonst keine zweite Grenze vorhanden
+%% Median, Min und Max, Interquartils-distance
+output_median = cell(1,length(midpoints));
+output_minmax = cell(1,length(midpoints));
+output_iqr = cell(1,length(midpoints));
+output_center = cell(1,length(midpoints));
+for i = 1:length(midpoints)
+    output_median{i} = median(output_intervals{i}(:,2)); % median
+    output_minmax{i} = [min(output_intervals{i}(:,2));max(output_intervals{i}(:,2))]; % minimum und maximum
+    output_iqr{i} = iqr(output_intervals{i}(:,2)); % interquartils-Abstand
+    output_center{i} = [prctile(output_intervals{i}(:,2),97.5);prctile(output_intervals{i}(:,2),2.5)]; % zentrale 95 Prozent der RMSD-Werte, obere und untere Grenze
 end
 
 %% output
@@ -132,8 +72,7 @@ output.median = output_median;
 output.minmax = output_minmax;
 output.iqr = output_iqr;
 output.center = output_center;
-output.midpoints = output_midpoints;
-
+output.midpoints = midpoints;
 
 end
 
