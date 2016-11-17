5ee3fa5d662af0b0c711636aad9c2794e1dd6a59
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2016-11-17 11:54:06 +0100

    Anmerkungen MS

--- dominik_applications/IntAndRMSD.m
+++ dominik_applications/IntAndRMSD.m
@@ -26,7 +26,7 @@ chm = 1; % mobile channel: Rot
 indices = cell(size(data)); % data aus data_spot_pairs
 nFrames = cell(size(indices));
 for m = 1:length(indices)
-    indices{m} = [vector(vector(:,1)==m,2)]; % hier die spot indices fuer diesen Film angeben
+    indices{m} = vector(vector(:,1)==m,2); % hier die spot indices fuer diesen Film angeben
     nFrames{m} = zeros(length(indices{m}),1);
     for s = 1:length(nFrames{m})
         %nFrames{m}(s) = min(floor(fit_cutoff{m,chm}(indices{m}(s))),length(data{m}{indices{m}(s),chm}.med_itrace));
@@ -54,8 +54,17 @@ intervals_comb = [];
 for i = 1:length(all_ints)
     intervals_comb = [intervals_comb; all_ints{i} all_rms10{i}]; % 2 Spalten Vektor mit Intensitaets-(1) und RMSD-Werten(2)
 end
-intervals_comb = sortrows(intervals_comb,1); % nach Intensitaet sortiert
-intervals_comb = intervals_comb(find(intervals_comb(:,1)>0):end,:); % alle Null-Eintraege werden wieder entfernt (keine Ahnung woher diese kommen)
+intervals_comb = sortrows(intervals_comb,1); % nach Intensitaet sortiert; MS: Dieser Sortierschritt ist unnoetig.
+% Ueberhaupt kannst du diesen Teil schon durch umschreiben der vorherigen
+% section erledigen. die all_ints und all_rms10 werden spaeter ja eh nicht
+% mehr verwendet, oder?
+
+intervals_comb = intervals_comb(intervals_comb(:,1)>0,:); % alle Null-Eintraege werden wieder entfernt (keine Ahnung woher diese kommen)
+
+%MS: Ich haette das gern so, dass am Rand abgeschnitten wird, also wirklich
+%nur die Punkte mitgenommen werden, die in einem der vorgegebenen
+%Intervalle liegen. Ausserdem: Was passiert hier mit den Werten, die genau
+%auf einer Intervallgrenze liegen?
 
 output_intervals = cell(1,length(intervals)+1);
 for i = 1 % erstes Intervall liegt links der ersten Grenze
@@ -68,6 +77,9 @@ for i = length(intervals)+1 % letztes Intervall liegt rechts von letzter Grenze;
     output_intervals{i} = intervals_comb(intervals_comb(:,1)>intervals(i-1),:);
 end
 
+%MS: probier doch mal aus, um welchen Faktor sich die Rechenzeit
+%reduziert, wenn du die folgenden drei oder vier for-Schleifen zu einer
+%Schleife kombinierst. Das geht z.B. mit tic und toc
 %% median
 output_median = cell(1,length(intervals)+1);
 for i = 1:length(intervals)+1
@@ -87,21 +99,32 @@ for i = 1:length(intervals)+1
 end
 
 %% center 95 percent
+% MS: Hier hatte ich mich evtl. unklar ausgedrueckt. Es reichen mir die
+% obere und untere Grenze des 95%-Intervalls, also diese ceil-/floor-Werte.
+% Sollte aber auch mit dem Befehl prctile gehen, oder?
+
 output_center = cell(1,length(intervals)+1);
 for i = 1:length(intervals)+1
     if ~isempty(output_intervals{i})
         variable = []; % Hilfsvariable
         variable = output_intervals{i};
         variable = sortrows(variable,2); % das Intervall nach RMSD-Werten sortieren und nur 95 Prozent auswaehlen
+        %MS: Gute Idee mit der Hilfsvariable, aber aus den vorherigen drei Schritten kannst du einen einzigen
+        %machen. Und es empfiehlt sich, solche Hilfsvariablen mit "tmp" zu
+        %benennen. Dann ist gleich klar, dass es nur eine 'Zwischenablage' ist.
         variable = variable(ceil(length(variable)*0.025):floor(length(variable)*0.975),:);   
-        output_center{i} = sortrows(variable,1); % wieder nach Intensitaet sortieren
+        output_center{i} = sortrows(variable,1); % wieder nach Intensitaet sortieren; MS: sind sie das nicht schon?!
+    end
 end
 
 %% interval midpoint
-output_midpoints = cell(1,length(intervals)+1);
+output_midpoints = cell(1,length(intervals)+1); %MS: Fuer einfache Zahlenwerte brauchst du kein cell array, da tut es auch ein normales array.
 output_midpoints{1} = [];
 output_midpoints{end} = [];
-abs = intervals(2)-intervals(1);
+abs = intervals(2)-intervals(1); %MS: Achtung mit abs, das gibt es schon als Befehl in Matlab (absolute value).
+%Hier wuerde auch 'delta' passen. Oder 'width'/'w'/'hw'. Und wenn du es gleich
+%halbierst, dann musst du das nicht in jeder Schleifeniteration tun. Wobei
+%es eh auch ohne Schleife geht, wenn du ein normales array benutzt.
 for i = 2:length(intervals)
     output_midpoints{i} = intervals(i)-abs*0.5; % Mittelpunkte nur von Intervall i+1 bis i-1, da sonst keine zweite Grenze vorhanden
 end
