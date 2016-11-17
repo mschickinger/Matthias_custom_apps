318eea030fd478efa14c2d408623ca1d7d59e7fe
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2016-11-17 18:58:47 +0100

    Aenderungen MS

--- dominik_applications/IntAndRMSD.m
+++ dominik_applications/IntAndRMSD.m
@@ -31,18 +31,19 @@ for m = 1:length(indices)
 end
 
 %% arrays
-all_ints = cell(size(indices));
-all_rms10 = cell(size(indices));
+%all_ints = cell(size(indices)); % obsolet, aber lassen wir es mal kommentiert drin...
+%all_rms10 = cell(size(indices));
+intervals_comb = zeros(sum(vertcat(nFrames{:}),2)); % MS: So zum Beispiel.
 counter = 0;
-intervals_comb = []; %zeros(length(vector)*27000,2); % koennte man auch sehr sehr gross machen und am Ende alle Nullen wieder herausloeschen, kA wie man sonst die Vektorgroesse sonst vorher angeben kann...
 for m = 1:length(indices)
-    all_ints{m} = zeros(sum(nFrames{m}),1);
-    all_rms10{m} = zeros(sum(nFrames{m}),1);
+    %all_ints{m} = zeros(sum(nFrames{m}),1); % obsolet, aber lassen wir es mal kommentiert drin...
+    %all_rms10{m} = zeros(sum(nFrames{m}),1);
     for s = 1:length(indices{m})
-        all_ints{m}(counter+(1:nFrames{m}(s))) = data{m}{indices{m}(s),chm}.itrace(1:nFrames{m}(s));
-        all_rms10{m}(counter+(1:nFrames{m}(s))) = data{m}{indices{m}(s),chm}.vwcm.rms10(1:nFrames{m}(s));
+        %all_ints{m}(counter+(1:nFrames{m}(s))) = data{m}{indices{m}(s),chm}.itrace(1:nFrames{m}(s));
+        %all_rms10{m}(counter+(1:nFrames{m}(s))) = data{m}{indices{m}(s),chm}.vwcm.rms10(1:nFrames{m}(s));
+        intervals_comb(counter+(1:nFrames{m}(s)),:) = [data{m}{indices{m}(s),chm}.itrace(1:nFrames{m}(s)) ...
+            data{m}{indices{m}(s),chm}.vwcm.rms10(1:nFrames{m}(s))];
         counter = counter + nFrames{m}(s);
-        intervals_comb = [intervals_comb; all_ints{m} all_rms10{m}];
     end
 end
 intervals_comb = intervals_comb(intervals_comb(:,1)>0,:); % alle Null-Eintraege werden wieder entfernt (keine Ahnung woher diese kommen)
@@ -54,25 +55,22 @@ for i = 1:length(midpoints) % z.B.: Intervall 1 entspricht 5000 bis 5199
     output_intervals{i} = intervals_comb(intervals_comb(:,1) >= (midpoints(i)-100) & intervals_comb(:,1) <= (midpoints(i)+99),:);
 end
 
+%% Output struct
+output = struct('midpoints', midpoints, 'intervals', intervals, ...
+    'median', zeros(1,length(midpoints)), ...
+    'minmax', zeros(2,length(midpoints)), ...
+    'iqr', zeros(1,length(midpoints)),...
+    'center95', zeros(2,length(midpoints)));
+
 %% Median, Min und Max, Interquartils-distance
-output_median = cell(1,length(midpoints));
-output_minmax = cell(1,length(midpoints));
-output_iqr = cell(1,length(midpoints));
-output_center = cell(1,length(midpoints));
-for i = 1:length(midpoints)
-    output_median{i} = median(output_intervals{i}(:,2)); % median
-    output_minmax{i} = [min(output_intervals{i}(:,2));max(output_intervals{i}(:,2))]; % minimum und maximum
-    output_iqr{i} = iqr(output_intervals{i}(:,2)); % interquartils-Abstand
-    output_center{i} = [prctile(output_intervals{i}(:,2),97.5);prctile(output_intervals{i}(:,2),2.5)]; % zentrale 95 Prozent der RMSD-Werte, obere und untere Grenze
+
+for i = 1:length(output.midpoints)
+    output.median(i) = median(output_intervals{i}(:,2)); % median
+    output.minmax(:,i) = [min(output_intervals{i}(:,2));max(output_intervals{i}(:,2))]; % minimum und maximum
+    output.iqr(i) = iqr(output_intervals{i}(:,2)); % interquartils-Abstand
+    output.center95(:,i) = [prctile(output_intervals{i}(:,2),97.5);prctile(output_intervals{i}(:,2),2.5)]; % zentrale 95 Prozent der RMSD-Werte, obere und untere Grenze
 end
 
-%% output
-output.intervals = output_intervals;
-output.median = output_median;
-output.minmax = output_minmax;
-output.iqr = output_iqr;
-output.center = output_center;
-output.midpoints = midpoints;
 
 end
 
