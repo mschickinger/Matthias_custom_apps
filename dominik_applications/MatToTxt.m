1c2bb8bc747b42c9069d7abd9d1c5527e6c735d7
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2016-11-22 21:05:46 +0100

    more generic

--- dominik_applications/MatToTxt.m
+++ dominik_applications/MatToTxt.m
@@ -1,16 +1,22 @@
-function [] = MatToTxt(vector,hop,data)
+function [] = MatToTxt(vector,DateAndSample,data)
 
-m = vector(:,1);
-s = vector(:,2);
-name = [];
+if iscell(DateAndSample.date)
+    date = DateAndSample.date{1};
+    sample = DateAndSample.sample{1};
+else
+    date = DateAndSample.date;
+    sample = DateAndSample.sample;
+end
+
+stem = inputdlg('Enter filename stem:', 'Stem', 1, {[date '_' sample]});
+stem = stem{1};
 
 for i = 1:size(vector,1)
-    name = [hop.date,'_',hop.sample,'_','m',num2str(m(i)),'s',num2str(s(i)),'.txt'];
-    inhalt = data{m(i)}{s(i),1}.vwcm.rms10;
+    name = ['m',num2str(vector(i,1)),'s',num2str(vector(i,2)),'_',stem,'.txt'];
+    inhalt = data{vector(i,1)}{vector(i,2),1}.vwcm.rms10;
     fileID = fopen(name,'w');
     fprintf(fileID,'%6.4f\r\n',inhalt);
     fclose(fileID);
-
 end
 end
 
