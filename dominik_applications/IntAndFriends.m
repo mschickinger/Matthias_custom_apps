79faa091ccd5ccc7875c803d918d1addf91199f9
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2016-12-09 18:12:43 +0100

    development

--- dominik_applications/IntAndFriends.m
+++ dominik_applications/IntAndFriends.m
@@ -14,35 +14,34 @@ midpoints   =
 p = inputParser;
 addRequired(p, 'data')
 addRequired(p, 'vector')
+addOptional(p, 'N', 200, @isscalar)
 
 parse(p, data, vector, varargin{:})
 data = p.Results.data;
 vector = p.Results.vector;
+N = p.Results.N; % Fensterbreite (+1)
 
 %% Rohdaten
 m = vector(1);
 s = vector(2);
 itrace = data{m}{s,1}.itrace;
-x = data{m}{s,1}.vwcm.pos(:,1);
-y = data{m}{s,1}.vwcm.pos(:,2);
-N = 200; % Fensterbreite
+x = data{m}{s,1}.vwcm.disp100(:,1);
+y = data{m}{s,1}.vwcm.disp100(:,2);
 
 %% 
 C = zeros(length(itrace)-N,2,2);
 I_mean = zeros(length(itrace)-N,1);
 I_median = zeros(length(itrace)-N,1);
-midpoints = zeros(length(itrace)-N,1);
 for i = 1:(length(itrace)-N)
-    C(i,:,:) = cov(x(i:i+N-1),y(i:i+N-1));
-    I_mean(i) = mean(itrace(i:i+N-1));
-    I_median(i) = median(itrace(i:i+N-1));
-    midpoints(i) = i+N/2-1;
+    C(i,:,:) = cov(x(i:i+N),y(i:i+N));
+    I_mean(i) = mean(itrace(i:i+N));
+    I_median(i) = median(itrace(i:i+N));
 end
 
 output.C = C;
 output.I_mean = I_mean;
 output.I_median = I_median;
-output.midpoints = midpoints;
+output.N = N;
 
 
 end
