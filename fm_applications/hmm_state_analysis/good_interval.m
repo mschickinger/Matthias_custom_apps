a0255f219424b4b2690d1c817091d513d7433b44
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2017-01-03 11:47:34 +0100

    initial commit 'good_interval.m'

---
+++ fm_applications/hmm_state_analysis/good_interval.m
@@ -0,0 +1,33 @@
+function interval = good_interval(XY,thr1,thr2)
+%   Find the best suited interval for HMM analysis in a XY-time-tracjectory
+
+%   Input:  (1) XY: 2xN trajectory of X- and Y-coordinates
+%           (2) thr1: threshold for strict rejection of frames
+%           (3) thr2: threshold for rejection of intervals with a certain
+%                       tolerance
+
+Lmin = 10000;
+
+% first round
+I = [0 find(max(XY,[],1) > thr1) size(XY,1)+1];
+L = diff(I) - 1;
+
+I = I(L>=Lmin) + 1;
+L = L(L>=Lmin);
+
+% second round
+tol = 0.001;
+keep = zeros(size(I));
+for i = 1:length(keep)
+    tmpD = max(XY(:,I(i)+(0:L(i)-1)),[],1);
+    keep(i) = sum(tmpD > thr2)/length(tmpD) <= tol;
+end
+I = I(keep==1);
+L = L(keep==1);
+[~,IND] = max(L);
+
+interval = [I(IND) I(IND)+L(IND)-1];
+
+
+end
+
