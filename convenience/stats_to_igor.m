f149137863443e7b7c4ee57d25f430972cde7fa4
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2017-01-18 13:55:06 +0100

    completion

--- convenience/stats_to_igor.m
+++ convenience/stats_to_igor.m
@@ -4,19 +4,19 @@ function [  ] = stats_to_igor( stats, ID, path )
 if nargin == 2
     path = cd;
 end
-wave_names = {[ID '_mTb'],[ID '_mTu']};
+wave_names = {[ID '_mTu'],[ID '_mTb'],[ID '_SEMu'],[ID '_SEMb']};
 fullpath = [path filesep ID '_Scatter.txt'];
-    if size(data,2)== length(wave_names)
+    if size(stats,2)== length(wave_names)
         % write header/wave names
         fileID=fopen(fullpath, 'w'); %open file to write
-        for i=1:size(data,2)            %write wavenames at each column header
+        for i=1:size(stats,2)            %write wavenames at each column header
             fprintf(fileID, [wave_names{i} '\t']);
         end
         fprintf(fileID,'\n');
         fclose(fileID);
 
         % append data
-        dlmwrite(file_location, data, 'delimiter', '\t','-append')
+        dlmwrite(fullpath, stats, 'delimiter', '\t','-append')
     else
         disp('Error: columns not equal to number of wave names.')
     end
