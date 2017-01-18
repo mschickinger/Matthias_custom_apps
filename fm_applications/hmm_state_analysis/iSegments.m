0a6a9cafbd16f3d99dd0fc48a54348e0c740974c
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2017-01-18 14:00:20 +0100

    output indices of segments also

--- fm_applications/hmm_state_analysis/iSegments.m
+++ fm_applications/hmm_state_analysis/iSegments.m
@@ -1,14 +1,17 @@
-function [ segments ] = iSegments( trace , thr )
+function [ segments, segments_idx ] = iSegments( trace , thr )
 
     thr = sort(thr);
     lasts = zeros(length(thr),1);
+    segments_idx = zeros(length(thr),1);
     for i = 1:length(thr)
         tmp = find(trace>=thr(i),1,'last');
         if ~isempty(tmp)
             lasts(i) = tmp;
+            segments_idx(i) = i;
         end
     end
     lasts = unique(nonzeros(lasts));
+    segments_idx = nonzeros(segments_idx);
     if ~isempty(lasts)
         firsts = [1; lasts(1:end-1)+1];
         segments = [firsts lasts];
