702523f1a0ddab1d9d7548fb32344ba3d378063c
mschickinger <matthiasschickinger@Giskardeventlov.dhcp.e14.ph.tum.de>
2017-08-30 19:28:17 +0200

    necessary bugfix

--- fm_applications/vector_functions/correctXYensemble.m
+++ fm_applications/vector_functions/correctXYensemble.m
@@ -13,20 +13,25 @@ for m = 1:M
     tmpMean = zeros(2,max(tmpIntervals(:,2))); 
     tmpN = zeros(1,max(tmpIntervals(:,2)));
     tmpL = size(tmpIntervals,1);
-    for i = 1:tmpL
-        counter = counter + 1;
-        tmpI = tmpIntervals(i,:);
-        tmpMean(:,tmpI(1):tmpI(2)) = tmpMean(:,tmpI(1):tmpI(2)) + xyIn{counter};
-        tmpN(tmpI(1):tmpI(2)) = tmpN(tmpI(1):tmpI(2)) + 1;
-    end
-    tmpMean = tmpMean(:,zeroFrame+1:end);
-    tmpN = tmpN(zeroFrame+1:end);
-    tmpMean = tmpMean./[tmpN;tmpN];
-    counter = counter - tmpL;
-    for i = 1:tmpL
-        counter = counter + 1;
-        tmpI = tmpIntervals(i,:);
-        xyOut{counter} = xyIn{counter} - tmpMean(:,(tmpI(1):tmpI(2))-zeroFrame);
+    if tmpL>=3
+        for i = 1:tmpL
+            counter = counter + 1;
+            tmpI = tmpIntervals(i,:);
+            tmpMean(:,tmpI(1):tmpI(2)) = tmpMean(:,tmpI(1):tmpI(2)) + xyIn{counter};
+            tmpN(tmpI(1):tmpI(2)) = tmpN(tmpI(1):tmpI(2)) + 1;
+        end
+        tmpMean = tmpMean(:,zeroFrame+1:end);
+        tmpN = tmpN(zeroFrame+1:end);
+        tmpMean = tmpMean./[tmpN;tmpN];
+        counter = counter - tmpL;    
+        for i = 1:tmpL
+            counter = counter + 1;
+            tmpI = tmpIntervals(i,:);
+            xyOut{counter} = xyIn{counter} - tmpMean(:,(tmpI(1):tmpI(2))-zeroFrame);
+        end
+    else
+        xyOut(counter+(1:tmpL)) = xyIn(counter+(1:tmpL));
+        counter = counter+tmpL;
     end
     meansCell{m} = tmpMean;
     nCell{m} = tmpN;
