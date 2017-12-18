c78bbff7a80edf4c168cc8497a705c90959f8fe5
mschickinger <m.schickinger@tum.de>
2017-12-18 13:38:23 +0100

    initial commit ErlangPercentiles.m

---
+++ fm_applications/data_sorting/ErlangPercentiles.m
@@ -0,0 +1,17 @@
+figure
+tau = 5;
+N = [5 10 50 100];
+ts = cell(numel(N),1);
+for p = 1:2
+    subplot(2,1,p)
+    hold off
+end
+for i = 1:numel(N)
+    ts{i} = linspace(0,5*N(i)*tau,10000);
+    subplot(2,1,1)
+    plot(ts{i}./N(i),N(i)*erlangpdf(ts{i},1./tau,N(i)))
+    hold on
+    subplot(2,1,2)
+    plot(ts{i}./N(i),erlangcdf(ts{i},1./tau,N(i)))
+    hold on
+end
