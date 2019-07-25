d9dbec44e0fdb7f250cb13d9a46b1ae98f53a776
mschickinger <m.schickinger@tum.de>
2019-07-25 13:01:09 +0200

    Update even_iEdges.m

--- fm_applications/vector_functions/even_iEdges.m
+++ fm_applications/vector_functions/even_iEdges.m
@@ -2,30 +2,32 @@ function [ output ] = even_iEdges (traces, N_threshs, min_thresh )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-threshs = min_thresh;
+thresholds = min_thresh;
 
 tmp = N_below(traces,min_thresh);
-disp(threshs)
-disp(tmp.N./tmp.N_all)
+%disp(thresholds)
+%disp(tmp.N./tmp.N_all)
     
 Pgood = 1-tmp.N/tmp.N_all;
 deltaP = Pgood/N_threshs;
 
 i = 2;
 while i<=N_threshs
-    threshs = [threshs threshs(i-1)+100];
-    tmp = N_below(traces,threshs);
-    disp(threshs)
-    disp(tmp.N./tmp.N_all)
+    thresholds = [thresholds thresholds(i-1)+100];
+    tmp = N_below(traces,thresholds);
+    %disp(threshs)
+    %disp(tmp.N./tmp.N_all)
     while tmp.N(i)/tmp.N_all < (tmp.N(1)/tmp.N_all+(i-1)*deltaP)
-        threshs(i) = threshs(i)+100;
-        tmp = N_below(traces,threshs);
-        disp(threshs)
-        disp(tmp.N./tmp.N_all)
+        thresholds(i) = thresholds(i)+100;
+        tmp = N_below(traces,thresholds);
+        %disp(threshs)
+        %disp(tmp.N./tmp.N_all)
     end
     i = i+1;
 end
-
+display(thresholds)
+fractions_below = tmp.N./tmp.N_all;
+display(fractions_below)
 output = tmp;
 end
 
